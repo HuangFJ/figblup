2e6c8a97731435f7bde370cc17ccc0f103a3a387
Jon Huang <biohfj@gmail.com>
2022-05-27 15:43:33 +0800

    add unit test

--- src/utils.rs
+++ src/utils.rs
@@ -4,51 +4,6 @@ use na::*;
 /// A B
 /// C D
 /// </pre>
-/// ```
-/// let r = compose_sqr_mat(
-///     dmatrix![
-///         1.1,2.0,3.0;
-///         4.0,5.0,6.0;
-///         7.0,8.0,9.1
-///     ],
-///     dmatrix![
-///         1.2,2.0;
-///         4.0,5.0;
-///         7.0,8.2
-///     ],
-///     dmatrix![
-///         1.3,2.0,3.0;
-///         4.0,5.0,6.3
-///     ],
-///     dmatrix![
-///         1.4,2.0;
-///         4.0,5.4
-///     ],
-/// );
-/// println!("{}", R);
-/// ```
-/// ```
-/// let r = compose_sqr_mat(
-///     dmatrix![
-///         1.2,2.0;
-///         4.0,5.2
-///     ],
-///     dmatrix![
-///         1.1,2.0,3.0;
-///         4.0,5.0,6.1
-///     ],
-///     dmatrix![
-///         1.4,2.0;
-///         4.0,5.0;
-///         7.0,8.4
-///     ],
-///     dmatrix![
-///         1.3,2.0,3.0;
-///         4.0,5.0,6.0;
-///         7.0,8.0,9.3
-///     ],
-/// );
-/// ```
 pub fn compose_sqr_mat(
     mut block_a: DMatrix<f64>,
     mut block_b: DMatrix<f64>,
@@ -100,6 +55,41 @@ pub fn compose_sqr_mat(
 }
 
 /// <pre>
+/// A
+/// C
+/// </pre>
+pub fn compose_v_mat(mut block_a: DMatrix<f64>, mut block_c: DMatrix<f64>) -> DMatrix<f64> {
+    assert!(block_a.ncols() == block_c.ncols());
+
+    if block_a.nrows() >= block_c.nrows() {
+        let ith = block_a.nrows();
+        block_a = block_a.insert_rows(ith, block_c.nrows(), 0.0);
+
+        for i in 0..block_c.nrows() {
+            block_a.set_row(ith + i, &block_c.row(i));
+        }
+        block_a
+    } else {
+        block_c = block_c.insert_rows(0, block_a.nrows(), 0.0);
+
+        for i in 0..block_a.nrows() {
+            block_c.set_row(i, &block_a.row(i));
+        }
+        block_c
+    }
+}
+
+/// <pre>
+/// A   B
+/// </pre>
+pub fn compose_h_mat(mut block_a: DMatrix<f64>, block_b: DMatrix<f64>) -> DMatrix<f64> {
+    assert!(block_a.nrows() == block_b.nrows());
+
+    block_a.extend(block_b.column_iter());
+    block_a
+}
+
+/// <pre>
 /// a
 /// c
 /// </pre>
@@ -119,5 +109,121 @@ pub fn compose_v_vec(vec_a: DVector<f64>, vec_c: DVector<f64>) -> DVector<f64> {
             vec.set_row(i, &vec_a.row(i));
         }
         vec
+    }
+}
+
+/// <pre>
+/// a   b
+/// </pre
+pub fn compose_h_vec(mut vec_a: RowDVector<f64>, vec_b: RowDVector<f64>) -> RowDVector<f64> {
+    vec_a.extend(vec_b.column_iter());
+    vec_a
+}
+
+#[cfg(all(test))]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mat_3_3 = dmatrix![
+            1.33,2.33,3.33;
+            4.33,5.33,6.33;
+            7.33,8.33,9.33
+        ];
+        let mat_3_2 = dmatrix![
+            1.32,2.32;
+            3.32,4.32;
+            5.32,6.32
+        ];
+        let mat_2_3 = dmatrix![
+            1.23,2.23,3.23;
+            4.23,5.23,6.23
+        ];
+        let mat_2_2 = dmatrix![
+            1.22,2.22;
+            3.22,4.22
+        ];
+
+        assert_eq!(
+            compose_sqr_mat(
+                mat_3_3.clone(),
+                mat_3_2.clone(),
+                mat_2_3.clone(),
+                mat_2_2.clone()
+            ),
+            dmatrix![
+                1.33,2.33,3.33,1.32,2.32;
+                4.33,5.33,6.33,3.32,4.32;
+                7.33,8.33,9.33,5.32,6.32;
+                1.23,2.23,3.23,1.22,2.22;
+                4.23,5.23,6.23,3.22,4.22
+            ]
+        );
+
+        assert_eq!(
+            compose_sqr_mat(
+                mat_2_2.clone(),
+                mat_2_3.clone(),
+                mat_3_2.clone(),
+                mat_3_3.clone()
+            ),
+            dmatrix![
+                1.22,2.22,1.23,2.23,3.23;
+                3.22,4.22,4.23,5.23,6.23;
+                1.32,2.32,1.33,2.33,3.33;
+                3.32,4.32,4.33,5.33,6.33;
+                5.32,6.32,7.33,8.33,9.33
+            ]
+        );
+
+        assert_eq!(
+            compose_v_mat(mat_3_3.clone(), mat_2_3.clone()),
+            dmatrix![
+                1.33,2.33,3.33;
+                4.33,5.33,6.33;
+                7.33,8.33,9.33;
+                1.23,2.23,3.23;
+                4.23,5.23,6.23
+            ]
+        );
+
+        assert_eq!(
+            compose_v_mat(mat_2_2.clone(), mat_3_2.clone()),
+            dmatrix![
+                1.22,2.22;
+                3.22,4.22;
+                1.32,2.32;
+                3.32,4.32;
+                5.32,6.32
+            ]
+        );
+
+        assert_eq!(
+            compose_h_mat(mat_2_3.clone(), mat_2_2.clone()),
+            dmatrix![
+                1.23,2.23,3.23,1.22,2.22;
+                4.23,5.23,6.23,3.22,4.22
+            ]
+        );
+
+        assert_eq!(
+            compose_h_vec(mat_3_3.row(0).clone_owned(), mat_3_3.row(1).clone_owned()),
+            dmatrix![1.33, 2.33, 3.33, 4.33, 5.33, 6.33]
+        );
+
+        assert_eq!(
+            compose_v_vec(
+                mat_2_2.column(0).into_owned(),
+                mat_3_3.column(0).into_owned()
+            ),
+            dmatrix![
+                1.22;
+                3.22;
+                1.33;
+                4.33;
+                7.33
+            ]
+        );
     }
 }
